9ca04cb5e04450835d44a1f95431d45b96b31752
Jose Carlos Velasco <jcvl0611@gmail.com>
2025-12-05 22:15:19 -0600

    feat: Ejercicio 3 terminado

---
+++ src/js/exercise2.js
@@ -0,0 +1,15 @@
+/*
+Exercise #2 (sugerencia map())
+Write a function that:
+Takes in an array of numbers.
+Doubles the value of each number in the array.
+Prints out the new updated array.
+Example: Given an array [1, 2, 4, 5]. The output should be [2, 4, 8, 10]
+
+*/
+
+
+const vectorX2 = vector => vector.map(duplicar); // El método map aplica la función  duplicar a cada elemento del arreglo por separado.
+const duplicar = num => num*2; // Cada elemento del arreglo pasa por esta funcion
+let numbers = [1, 2, 4, 5];
+console.log(vectorX2(numbers));
